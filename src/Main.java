1a7d98a26e64609fc27351be5806dc1419e29e6b
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 21:10:11 +0100

    CW6

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int x = method1(1,2);
-        int y = method1(7,8,9);
-        //Pozwolilem sobie uzyc zmiennych poniewaz w przykladzie zadania ich nie wykorzystujemy..
-        System.out.println("wynik: "+ (x + y));
+        List<String> list1 = new ArrayList<>();
+
+        AddArrayListFor(list1, 8);
+        RemoveLastFromList(list1 , 2);
+        ShowAllFromArrayOnConsole(list1);
+        AddArrayListFor(list1, 3);
+        System.out.println(list1.size());
+        ReverseArray(list1);
+        ShowAllFromArrayOnConsole(list1);
     }
-    public static Integer method1(Integer vrA, Integer vrB){
-        return vrA + vrB;
+    public static void ShowAllFromArrayOnConsole(List<String> l) {
+        for(String s : l ){
+            System.out.print(s+" ");
+        }
+        System.out.println();
     }
-    public static Integer method1(Integer vrA , Integer vrB , Integer vrC){
-        return vrA + vrB + vrC;
+    public static void RemoveLastFromList(List<String> l , int howManyDelete) {
+        for(int i=0; i<howManyDelete; i++) {
+            l.remove((l.size())-1);
+        }
     }
+    public static void AddArrayListFor(List<String> l, int howManyAdd){
+        Scanner input = new Scanner(System.in);
+        for(int i=0; i<howManyAdd; i++){
+            l.add(input.nextLine());
+        }
+    }
+    public static void ReverseArray(List<String> l){
+        Collections.reverse(l);
+    }
+
 }
-
-
