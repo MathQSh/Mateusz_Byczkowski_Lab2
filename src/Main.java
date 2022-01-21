fe1dad0c1a2e6f55c2dfcdaa2c17c5f9189a9175
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 20:31:32 +0100

    CW1_ZAD2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 1000;
-        int x = 26;
-        while (i > 0){
-            if (i%x==0)
-                System.out.println("liczba "+ i + " podzielna przez "+ x);
+        int[] tab1 = {1, 2, 9, 7, 12};
+        int i = 4;
+        while (i >= 0){
+            System.out.println("liczba "+ tab1[i]);
             i--;
 
         }
