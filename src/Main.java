46067fcf9950574281513efd996bb870964ece6b
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 20:57:12 +0100

    CW3

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
         System.out.print("Podaj liczbę:");
         int x = scan.nextInt();
         switch (x % 2) {
-            case 0 -> System.out.println("Parzysta");
-            default -> System.out.println("Nieparzysta");
+            case 0 -> System.out.println("Liczba " + x + " jest parzysta");
+            default -> System.out.println("Liczba " + x + " jest nieparzysta");
+        }
+        switch (x % 7){
+            case 0 -> System.out.println("Liczba " + x + " jest podzielna przez 7");
+            case 1 -> System.out.println("Liczba " + x + " jest niepodzielna przez 7");
         }
     }
 }
