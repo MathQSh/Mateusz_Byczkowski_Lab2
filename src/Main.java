ff565bd159148fd224dd07bc91e8d83b7a773d93
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 21:10:41 +0100

    CW6_ZAD2

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,24 @@
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
-        List<String> list1 = new ArrayList<>();
+        Set<Integer> set1 = new TreeSet<>();
 
-        AddArrayListFor(list1, 8);
-        RemoveLastFromList(list1 , 2);
-        ShowAllFromArrayOnConsole(list1);
-        AddArrayListFor(list1, 3);
-        System.out.println(list1.size());
-        ReverseArray(list1);
-        ShowAllFromArrayOnConsole(list1);
+        AddToSetList(set1);
+        Iterator(set1);
     }
-    public static void ShowAllFromArrayOnConsole(List<String> l) {
-        for(String s : l ){
-            System.out.print(s+" ");
+    public static void AddToSetList(Set<Integer> set) {
+        Scanner input = new Scanner(System.in);
+        for(int i=0; i<10; i++){
+            set.add(input.nextInt());
+        }
+    }
+    public static void Iterator(Set<Integer> set ) {
+        for (Integer i : set) {
+            System.out.print(i + ", ");
         }
         System.out.println();
     }
-    public static void RemoveLastFromList(List<String> l , int howManyDelete) {
-        for(int i=0; i<howManyDelete; i++) {
-            l.remove((l.size())-1);
-        }
-    }
-    public static void AddArrayListFor(List<String> l, int howManyAdd){
-        Scanner input = new Scanner(System.in);
-        for(int i=0; i<howManyAdd; i++){
-            l.add(input.nextLine());
-        }
-    }
-    public static void ReverseArray(List<String> l){
-        Collections.reverse(l);
-    }
-
 }
+
