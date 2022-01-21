7e61d7c4f338ace926829e3ded8f8adf4c041bbf
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 20:30:28 +0100

    CW1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1000;
+        int x = 26;
+        while (i > 0){
+            if (i%x==0)
+                System.out.println("liczba "+ i + " podzielna przez "+ x);
+            i--;
+
+        }
+    }
+}
