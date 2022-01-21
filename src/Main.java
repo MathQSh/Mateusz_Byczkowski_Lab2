baa5161f08d99a3bcbff7dbb057a2c84526cdc66
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 20:46:52 +0100

    CW3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int[] numbers = new int[5];
-        for(int i=0;i<5;i++){
-            numbers[i] = in.nextInt()+11;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbę:");
+        int x = scan.nextInt();
+        switch (x % 2) {
+            case 0 -> System.out.println("Parzysta");
+            default -> System.out.println("Nieparzysta");
         }
-
-        Arrays.stream(numbers, 0, 5).forEach(System.out::println);
     }
 }
