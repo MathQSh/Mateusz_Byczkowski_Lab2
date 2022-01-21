9a5ad986bc310f61979aef61621dcab0754b1ccf
bycior5a@wp.pl <bycior5a@wp.pl>
2022-01-21 20:59:47 +0100

    CW3_ZAD2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Podaj liczbę:");
-        int x = scan.nextInt();
-        switch (x % 2) {
-            case 0 -> System.out.println("Parzysta");
-            default -> System.out.println("Nieparzysta");
-        }
+        String someString = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int length = someString.length();
+        long count = someString.chars().filter(ch -> ch == 'o').count();
+        long count2 = someString.codePoints().filter(ch -> ch == 'p').count();
+        long count3 = someString.chars().filter(ch -> ch == 'z').count();
+        long count4 = someString.chars().filter(ch -> ch == 'x').count();
+        long count5 = someString.chars().filter(ch -> ch == 'q').count();
+        System.out.println("Liter 'o' w danym wyrażeniu jest: "+count);
+        System.out.println("Liter 'p' w danym wyrażeniu jest: "+count2);
+        System.out.println("Liter 'z' w danym wyrażeniu jest: "+count3);
+        System.out.println("Liter 'x' w danym wyrażeniu jest: "+count4);
+        System.out.println("Liter 'q' w danym wyrażeniu jest: "+count5);
+        System.out.println("Suma wszystkich wyszukiwanych Liter: "+(count+count2+count3+count4+count5));
+        System.out.println("Suma pozostałych liter: "+(length-(count+count2+count3+count4+count5)));
     }
 }
